2591a91b527b1d5db4175fb19b5b9d0dadebe78a
kbinani <kbinani.bt@gmail.com>
2015-01-09 01:14:09 +0900

    Add optional runtime argument "--password"

--- bin/nicolive-dl.rb
+++ bin/nicolive-dl.rb
@@ -7,11 +7,13 @@ require "optparse"
 email = nil
 lvid = nil
 file = nil
+password = nil
 
 opt = OptionParser.new
 opt.on("-e EMAIL", "--email=EMAIL", "login email") { |v| email = v }
 opt.on("-l LIVEID", "--liveid=LIVEID", "nicolive id (ex: lv12345)") { |v| lvid = v }
 opt.on("-o FILE", "--output=FILE", "output file path") { |v| file = v }
+opt.on("-p PASSWORD", "--password=PASSWORD", "login password") { |v| password = v }
 
 opt.parse!(ARGV)
 
@@ -21,8 +23,10 @@ if email.nil? or lvid.nil? or file.nil?
 end
 
 begin
-  print "Enter password for user: #{email}\n"
-  password = STDIN.noecho(&:gets).chomp
+  if password.nil?
+    print "Enter password for user: #{email}\n"
+    password = STDIN.noecho(&:gets).chomp
+  end
 
   nico = NicoLiveDownloader.new
   nico.login(email, password)
